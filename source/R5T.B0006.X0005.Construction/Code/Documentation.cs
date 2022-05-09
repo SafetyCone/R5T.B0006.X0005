f3a6dfae432b81b03cc2f58d8fd4e94695ffe240
davidcoats <david@getsafetycone.com>
2022-05-09 12:28:54 -0700

    Initial commit of source-stub.

---
+++ source/R5T.B0006.X0005.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.B0006.X0005.Construction
+{
+    /// <summary>
+    /// Construction project for ISyntaxTextConstant base extensions for common text constants for Roslyn syntax elements..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.B0006.X0005/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.B0006.X0005
+{
+    /// <summary>
+    /// ISyntaxTextConstant base extensions for common text constants for Roslyn syntax elements.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}

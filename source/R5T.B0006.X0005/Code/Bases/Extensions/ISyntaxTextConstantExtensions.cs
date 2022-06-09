95ded524671aaf3ee6aafe671bc100091cd445da
davidcoats <david@getsafetycone.com>
2022-06-09 12:16:59 -0700

    Interim changes.

---
+++ source/R5T.B0006.X0005/Code/Bases/Extensions/ISyntaxTextConstantExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using R5T.B0006;
+
+using Instances = R5T.B0006.X0005.Instances;
+
+
+namespace System
+{
+    public static class ISyntaxTextConstantExtensions
+    {
+        public static string UsingSystemNamespace(this ISyntaxTextConstant _)
+        {
+            var output = Instances.SyntaxTextValues.UsingSystemNamespace;
+            return output;
+        }
+    }
+}
---
+++ source/R5T.B0006.X0005/Code/Instances.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using R5T.Z0003;
+
+
+namespace R5T.B0006.X0005
+{
+    public static class Instances
+    {
+        public static ISyntaxTextValues SyntaxTextValues { get; } = Z0003.SyntaxTextValues.Instance;
+    }
+}
